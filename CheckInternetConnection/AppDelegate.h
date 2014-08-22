d8f1c78a1c5ac13fbf2ef79b0b7dde814aa8acb1
Administrator <administrator@Mac112.local>
2014-08-23 00:09:25 +0530

    Initial Commit

---
+++ CheckInternetConnection/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CheckInternetConnection
+//
+//  Created by Administrator on 8/23/14.
+//  Copyright (c) 2014 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CheckInternetConnection/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CheckInternetConnection
+//
+//  Created by Administrator on 8/23/14.
+//  Copyright (c) 2014 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
